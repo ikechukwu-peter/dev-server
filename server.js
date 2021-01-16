82d3db743f9d3d9e9e67ab885b675f8f03555d9c
Peter <pitiyke@gmail.com>
2021-01-16 11:23:16 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config({ path: "./config.env" });
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "10kb" }));
+const port = process.env.PORT || 5000;
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Mongo Connected");
+  })
+  .catch((err) => console.log(err));
+
+//Passport middleware
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+app.listen(port, console.log("Listening on port 5000"));
